8d9114815e27cfe48218c057e2cb8a538ae54680
Bijay Regmi <wilson_blackman@yahoo.com>
2020-12-02 22:25:23 +0100

    first commit

---
+++ aligner.py
@@ -0,0 +1,11 @@
+import sys
+from argparse import ArgumentParser
+
+def main(argv):
+       parser = ArgumentParser()
+       parser.add_argument()
+       args = parser.parse_args()
+
+
+if __name__ = "__main__":
+    main(sys.argv[1:])
